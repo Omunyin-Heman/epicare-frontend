a68764bca9e21fb08914202cebf4b7c9452f32d7
omunyin heman <omunyinheheman@gmail.com>
2025-10-15 19:45:01 +0300

    Fix: export API_BASE_URL properly

--- src/config.jsx
+++ src/config.jsx
@@ -1,6 +1,6 @@
 const API_BASE_URL =
-  process.env.NODE_ENV === "production"
+  import.meta.env.MODE === "production"
     ? "https://my-backend-1-8oq8.onrender.com"
     : "http://127.0.0.1:8000";
 
-export default API_BASE_URL;
+export { API_BASE_URL };
